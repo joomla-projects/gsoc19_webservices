616b55cc2a946f4f87e670552907a06e4ba0bd7b
zero-24 <zero-24@users.noreply.github.com>
2017-06-17 17:51:22 +0200

    add file that @cokencorn commited to the main branch

---
+++ libraries/api/application/api.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @package    Joomla.Api
+ *
+ * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Joomla API Application class
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+final class JApplicationApi
+{
+
+
+}
