0b2df41413f6d4b0aaebafa6c4f967f8c9495ef5
wilsonge <georgejameswilson@googlemail.com>
2017-08-06 17:16:11 +0100

    Add missing Service Entry for the API App

--- libraries/src/CMS/Service/Provider/Application.php
+++ libraries/src/CMS/Service/Provider/Application.php
@@ -13,6 +13,7 @@ defined('JPATH_PLATFORM') or die;
 
 use JFactory;
 use Joomla\CMS\Application\AdministratorApplication;
+use Joomla\CMS\Application\ApiApplication;
 use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Log\Log;
 use Joomla\DI\Container;
@@ -62,6 +63,27 @@ class Application implements ServiceProviderInterface
 			function (Container $container)
 			{
 				$app = new SiteApplication(null, null, null, $container);
+
+				// The session service provider needs JFactory::$application, set it if still null
+				if (JFactory::$application === null)
+				{
+					JFactory::$application = $app;
+				}
+
+				$app->setDispatcher($container->get('Joomla\Event\DispatcherInterface'));
+				$app->setLogger(Log::createDelegatedLogger());
+				$app->setSession($container->get('Joomla\Session\SessionInterface'));
+
+				return $app;
+			},
+			true
+		);
+
+		$container->share(
+			'JApplicationApi',
+			function (Container $container)
+			{
+				$app = new ApiApplication(null, null, null, $container);
 
 				// The session service provider needs JFactory::$application, set it if still null
 				if (JFactory::$application === null)
