7301e4b57f2ebe3622d12f066a3dbf1d7d0f2730
muhakh <muhakh2@gmail.com>
2017-08-19 15:22:58 +0200

    Added displayList and displayItem tasks

--- libraries/src/CMS/Controller/Api.php
+++ libraries/src/CMS/Controller/Api.php
@@ -91,6 +91,58 @@ class Api extends Controller
 	}
 
 	/**
+	 * Typical view method for MVC based architecture
+	 *
+	 * This function is provide as a default implementation, in most cases
+	 * you will need to override it in your own controllers.
+	 *
+	 * @param   boolean  $cachable   If true, the view output will be cached
+	 * @param   array    $urlparams  An array of safe url parameters and their variable types, for valid values see {@link \JFilterInput::clean()}.
+	 *
+	 * @return  static  A \JControllerLegacy object to support chaining.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function displayItem($cachable = false, $urlparams = array())
+	{
+		if (empty($this->input->get('view')))
+		{
+			$viewname = $this->getName() . 'item';
+			$this->input->set('view', $viewname);
+		}
+
+		parent::display($cachable, $urlparams);
+
+		return $this;
+	}
+
+	/**
+	 * Typical view method for MVC based architecture
+	 *
+	 * This function is provide as a default implementation, in most cases
+	 * you will need to override it in your own controllers.
+	 *
+	 * @param   boolean  $cachable   If true, the view output will be cached
+	 * @param   array    $urlparams  An array of safe url parameters and their variable types, for valid values see {@link \JFilterInput::clean()}.
+	 *
+	 * @return  static  A \JControllerLegacy object to support chaining.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function displayList($cachable = false, $urlparams = array())
+	{
+		if (empty($this->input->get('view')))
+		{
+			$viewname = $this->getName() . 'list';
+			$this->input->set('view', $viewname);
+		}
+
+		parent::display($cachable, $urlparams);
+
+		return $this;
+	}
+
+	/**
 	 * Removes an item.
 	 *
 	 * @return  void
